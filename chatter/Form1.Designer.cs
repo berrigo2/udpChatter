e2f9a1d948977a955e042e7bf6f8082cc3bfd251
Brian <berrigo2@gmail.com>
2014-04-13 23:40:26 -0400

    -project started
    -only reciees 1 packet. lets figure it out

---
+++ chatter/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace chatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convoBox = new System.Windows.Forms.TextBox();
+            this.typingBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ipBox = new System.Windows.Forms.TextBox();
+            this.portBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.portBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // convoBox
+            // 
+            this.convoBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.convoBox.Location = new System.Drawing.Point(12, 35);
+            this.convoBox.Multiline = true;
+            this.convoBox.Name = "convoBox";
+            this.convoBox.ReadOnly = true;
+            this.convoBox.Size = new System.Drawing.Size(511, 260);
+            this.convoBox.TabIndex = 0;
+            // 
+            // typingBox
+            // 
+            this.typingBox.Location = new System.Drawing.Point(12, 301);
+            this.typingBox.Multiline = true;
+            this.typingBox.Name = "typingBox";
+            this.typingBox.Size = new System.Drawing.Size(511, 142);
+            this.typingBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "I.P.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Port";
+            // 
+            // ipBox
+            // 
+            this.ipBox.Location = new System.Drawing.Point(46, 3);
+            this.ipBox.Name = "ipBox";
+            this.ipBox.Size = new System.Drawing.Size(235, 20);
+            this.ipBox.TabIndex = 8;
+            this.ipBox.TextChanged += new System.EventHandler(this.sendToInfoChanged);
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(319, 3);
+            this.portBox.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(204, 20);
+            this.portBox.TabIndex = 9;
+            this.portBox.ValueChanged += new System.EventHandler(this.portBox_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 484);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.ipBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.typingBox);
+            this.Controls.Add(this.convoBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.portBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox convoBox;
+        private System.Windows.Forms.TextBox typingBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ipBox;
+        private System.Windows.Forms.NumericUpDown portBox;
+    }
+}
+
---
+++ chatter/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace chatter
+{
+    public partial class Form1 : Form
+    {
+        #region comments
+        // Create a socket object. This is the fundamental device used to network
+        // communications. When creating this object we specify:
+        // Internetwork: We use the internet communications protocol
+        // Dgram: We use datagrams or broadcast to everyone rather than send to
+        // a specific listener
+        // UDP: the messages are to be formated as user datagram protocal.
+        // The last two seem to be a bit redundant.
+        #endregion
+        Socket sendingSocket;
+        #region comments
+        // create an address object and populate it with the IP address that we will use
+        // in sending at data to. This particular address ends in 255 meaning we will send
+        // to all devices whose address begins with 192.168.2.
+        // However, objects of class IPAddress have other properties. In particular, the
+        // property AddressFamily. Does this constructor examine the IP address being
+        // passed in, determine that this is IPv4 and set the field. If so, the notes
+        // in the help file should say so.
+        #endregion
+        IPAddress sendToAddress;
+        IPAddress localIpAddress;
+        Int32 sendPort;
+        Int32 listenPort;
+        #region comments
+        // IPEndPoint appears (to me) to be a class defining the first or final data
+        // object in the process of sending or receiving a communications packet. It
+        // holds the address to send to or receive from and the port to be used. We create
+        // this one using the address just built in the previous line, and adding in the
+        // port number. As this will be a broadcase message, I don't know what role the
+        // port number plays in this.
+        #endregion
+        IPEndPoint sendingEndPoint;
+        #region comments
+        // The below three lines of code will not work. They appear to load
+        // the variable broadcast_string witha broadcast address. But that
+        // address causes an exception when performing the send.
+        //
+        //string broadcast_string = IPAddress.Broadcast.ToString();
+        //Console.WriteLine("broadcast_string contains {0}", broadcast_string);
+        //send_to_address = IPAddress.Parse(broadcast_string);
+        #endregion
+        UdpClient listener;
+        BackgroundWorker listenLoop;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listenPort = 6050;
+            portBox.ValueChanged += sendToInfoChanged;
+            ipBox.Text = "127.0.0.1";
+
+            portBox.Value = 6050;
+
+            sendingSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            convoBox.AppendText("type in lower box and press send to chat\n");
+            sendingSocket.Blocking = false;
+            listener = new UdpClient(listenPort, AddressFamily.InterNetwork);
+            listenLoop = new BackgroundWorker();
+            localIpAddress = getLocalIp();
+            //sendingEndPoint = new IPEndPoint(sendToAddress, sendPort);
+            listenLoop.DoWork += delegate
+            {
+                while (true)
+                {
+                    var recieve_byte_array = listener.Receive(ref sendingEndPoint);
+                    string recieved = Encoding.ASCII.GetString(recieve_byte_array, 0, recieve_byte_array.Length);
+                    convoBox.Invoke((MethodInvoker)(() => convoBox.AppendText("remote: " + recieved + "\n")));
+                }
+            };
+            listenLoop.RunWorkerAsync();
+        }
+
+        private IPAddress getLocalIp()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            return host.AddressList[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                convoBox.AppendText("local: " + typingBox.Text + "\n");
+                var bytes_to_send = Encoding.ASCII.GetBytes(typingBox.Text);
+                typingBox.Text = "";
+                sendingSocket.SendTo(bytes_to_send, sendingEndPoint);
+            }
+            catch (Exception ex)
+            {
+                convoBox.AppendText("failed to send string:" + ex.GetHashCode().ToString() + "\n");
+            }
+        }
+
+        private void sendToInfoChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                sendToAddress = IPAddress.Parse(ipBox.Text);
+                sendingEndPoint = new IPEndPoint(sendToAddress, Convert.ToInt32(portBox.Value));
+            }
+            catch { }
+        }
+
+        private void portBox_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                sendPort = Convert.ToInt32(portBox.Value);
+            }
+            catch { }
+        }
+    }
+}
